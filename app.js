e0c0e324e7c139ea159c57fa44061cb80f7f1381
jeovied <jonathan.e.oviedo@gmail.com>
2021-05-28 15:53:26 -0300

    modificando req y res

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require ('express');
 const app = express();
 const port = 3000;
 
+//  req -->>> request (peticion)
+//  res -->>> response (respuesta)
+//SIEMPRE el request y el response, van como parametro del callback que incluye app.get
+
+app.get('/',(req,res) => res.send('Por fin llegamos al navegador')) //ruta raiz de localhost:3000
+
 app.listen(port, () => console.log("Servidor corriendo en el puerto " + port));
 
 /* app.listen(3000), function(){
